06f69d8ea3900632187e783bf3a670c986f654e7
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2026-05-26 00:05:24 -0700

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> curr{};
+        dfs(0, curr, nums);
+        return res;
+    }
+private:
+vector<vector<int>> res{};
+
+void dfs(int n, vector<int>& curr, const vector<int>& nums) {
+    if (n >= nums.size()) {
+        res.push_back(curr);
+        return;
+    }
+    
+    curr.push_back(nums[n]);
+    dfs(n + 1, curr, nums);
+    curr.pop_back();
+    dfs(n + 1, curr, nums);
+}
+};
