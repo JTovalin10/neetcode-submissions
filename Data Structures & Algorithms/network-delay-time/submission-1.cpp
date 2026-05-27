f875ab9a0f2cce71936333b7561755b053575f93
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2026-05-27 16:48:23 -0700

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<int> dist(n + 1, INT_MAX);
+        dist[k] = 0;
+        for (int i = 0; i < (n - 1); i++) {
+            for (const auto& time : times) {
+                int src = time[0], dst = time[1], wei = time[2];
+                if (dist[src] != INT_MAX && dist[src] + wei < dist[dst]) {
+                    dist[dst] = dist[src] + wei;
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            res = std::max(res, dist[i]);
+        }
+        return res == INT_MAX ? -1 : res;
+    }
+};
