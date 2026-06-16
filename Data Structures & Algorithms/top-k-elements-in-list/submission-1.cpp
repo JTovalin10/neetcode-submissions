1dadc7847918abf8d1bc994480e4c3c78e6e9158
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2026-06-15 23:07:43 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // num -> count
+        unordered_map<int, int> counts{};
+        counts.reserve(nums.size());
+        for (const int num : nums) {
+            counts[num]++;
+        }
+
+        // count -> nums
+        unordered_map<int, vector<int>> bucket{};
+        bucket.reserve(counts.size());
+        for (const auto& [num, count] : counts) {
+            bucket[count].push_back(num);
+        }
+        // populate the result
+        vector<int> res{};
+        res.reserve(k);
+        for (auto i{nums.size()} ; i >= 0 && res.size() < k; i--) {
+            for (const int num : bucket[i]) {
+                if (res.size() == k) return res;
+                res.push_back(num);
+            }
+        }
+         return res;
+    }
+};
